22952ed6949f3f4793666be547089334042a3f9c
Rahulraju016 <rahulreigns16@gmail.com>
2018-11-05 21:41:32 +0530

    Added Comments

--- code.py
+++ code.py
@@ -13,15 +13,15 @@ def cost(b, m, data_points):
     return r_squared / (2 * float(len(data_points)))
 
 
-def gradient_step(b, m, data_points, alpha):
+def gradient_step(b, m, data_points, alpha): # Calculates Gradient for 1 iteration.
     b_grad = 0
     m_grad = 0
     N = float(len(data_points))
     for i in range(len(data_points)):
         X = data_points[i, 0]
         y = data_points[i, 1]
-        b_grad += -(1/N) * (y - ((m * X) + b))
-        m_grad += -(1/N) * X * (y - ((m * X) + b))
+        b_grad += -(1/N) * (y - ((m * X) + b)) # partial derivative of Cost with respect to slope - 'b'.
+        m_grad += -(1/N) * (X * (y - ((m * X) + b))) # partial derivative of Cost with respect to slope - 'm'.
     new_b = b - (alpha * b_grad)
     new_m = m - (alpha * m_grad)
     return [new_b, new_m]
@@ -30,19 +30,19 @@ def gradient_step(b, m, data_points, alpha):
 def batch_gradient_descent(data_points, initial_b, initial_m, alpha, iterations):
     b = initial_b
     m = initial_m
-    for i in range(iterations):
+    for i in range(iterations): # Calcultes Gradient Descent for n-iterations.
         b, m = gradient_step(b, m, array(data_points), alpha)
     return [b, m]
 
 
 def predict_y(X):
-    b, m = main()
-    y_pred = m * X + b
+    b, m = main()  # Gets the final slope and y-intercept after fitting the data.
+    y_pred = m * X + b # Calculates y with converged 'm' and 'b'.
     print(f"The regressor prediction for input 'X' = {X} is 'y' = {y_pred}")
 
 
 def main():
-    data_points = genfromtxt("data.csv", delimiter=",")
+    data_points = genfromtxt("data.csv", delimiter=",") # Reads the data sheet.
 
     alpha = 0.0001  # Learning Rate
 
@@ -52,20 +52,20 @@ def main():
     initial_b = 0
     iterations = 1000
 
-    start_cost = cost(initial_b, initial_m, data_points)
+    start_cost = cost(initial_b, initial_m, data_points) # Computes Cost before Gradient Descent.
     print(
         f"Starting Gradient Descent at 'y-intercept' = {initial_b}, 'slope' = {initial_m}, 'cost' = {start_cost}")
 
     [b, m] = batch_gradient_descent(
         data_points, initial_b, initial_m, alpha, iterations)
 
-    end_cost = cost(b, m, data_points)
+    end_cost = cost(b, m, data_points) # Computes cost after Gradient Descent.
     print(
         f"After performing Gradient Descent for {iterations} iterations we got: 'y-intercept' = {b}, 'slope' = {m}, 'cost' = {end_cost}")
 
-    return b, m
+    return b, m # return the slope and y-intercept after the Gradient Descent, in order to predict for the new values.
 
 
-if __name__ == "__main__":
+if __name__ == "__main__": # This is used to call the imported packages,it saves the memory.
     main()
-    # predict_y(59.813207869512318)
+    # predict_y(59.813207869512318) # original_y: 87.230925133687393,predicted_y: 88.48279754569299
